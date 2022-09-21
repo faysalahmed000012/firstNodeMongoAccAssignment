77b97ea3fb2275a7702babeb461a50d562f1409e
faysal ahmed <faysal00012@gmail.com>
2022-09-21 15:46:57 +0600

    https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const fs = require("fs");
-const { json } = require("express");
+// const { json } = require("express");
 
 const app = express();
 app.use(cors());
